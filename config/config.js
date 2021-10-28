985102a6ff448174c7e00ca3ab8df405b3b8ef08
emon-swe-sust <mahfuzur03@student.sust.edu>
2021-10-28 14:10:32 +0600

    create,update,delete,read by user id implemented

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ export const PORT = 4000;
 export const environment = {
     development: {
         serverURL: `http://localhost:${PORT}/`,
-        dbString: 'mongodb+srv://root:anik0104@cluster0.hgvwf.mongodb.net/users-crud?retryWrites=true&w=majority'
+        dbString: 'mongodb://localhost:27017/hapi'
     },
     production: {
         serverURL: `http://localhost:${PORT}/`,
