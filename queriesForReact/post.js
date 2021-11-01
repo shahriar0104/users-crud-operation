9de1a23a663d1bca15eeb986432a52110df1d89a
emon-swe-sust <mahfuzur03@student.sust.edu>
2021-11-01 17:14:11 +0600

    queriesForReact updated

--- queriesForReact/post.js
+++ queriesForReact/post.js
@@ -1,45 +1,3 @@
-
-// query findPost($PostId:PostId){
-//     getPostById(postId:$PostId){
-//       title
-//       details
-//       comments{
-//         commentor
-//         commentDetails
-//       }
-//       }
-//     }
-//   }
-
-//   {
-//     "PostId":{
-//       "_id":"617e429d82577ddad5d78eb5"
-//     }
-//   }
-
-
-
-// mutation addPost($post: PostInput){
-//     createPost(postInput: $post) {
-//       title
-//       details
-//       comments{
-//         postId
-//         commentor
-//         commentDetails
-//       }
-//     }
-//   }
-
-//   {
-//     "post":{
-//       "title":"first post",
-//       "details":"Allah vorosha"
-//     }
-//   }
-
-
-
 // query findAllpost{
 //     getAllPost {
 //       title
@@ -62,7 +20,42 @@
 //   }
 
 
+// query findPost($PostId:PostId){
+//     getPostById(postId:$PostId){
+//       title
+//       details
+//       comments{
+//         commentor
+//         commentDetails
+//       }
+//       }
+//     }
+//   }
 
+//   {
+//     "PostId":{
+//       "_id":"617e429d82577ddad5d78eb5"
+//     }
+//   }
+
+// mutation addPost($post: PostInput){
+//     createPost(postInput: $post) {
+//       title
+//       details
+//       comments{
+//         postId
+//         commentor
+//         commentDetails
+//       }
+//     }
+//   }
+
+//   {
+//     "post":{
+//       "title":"first post",
+//       "details":"Allah vorosha"
+//     }
+//   }
 
 // mutation upDatePost($postId: PostId, $update: UpdatedDetails){
 //     updatePost(postId:$postId, updatedDetails: $update)
@@ -74,6 +67,17 @@
 //     },
 //     "update":{
 //       "details":"update details"
+//     }
+//   }
+
+
+// mutation deletePost($postId: PostId){
+//     deletePost(postId:$postId)
+//   }
+
+//   {
+//     "postId":{
+//       "_id":"617e77956ec1dd9d88781608"
 //     }
 //   }
 
@@ -92,5 +96,35 @@
 //       "postId":"617e63750414e6532bb1c6d7",
 //       "commentor":"randoom second post",
 //       "commentDetails":"comment checking"
+//     }
+//   }
+
+
+
+// mutation addUser($users: CreateUserInput){
+//     createUser(createUserInput: $users){
+//       username
+//     }
+//   }
+
+//   {
+//     "users":{
+//       "username":"emon",
+//       "email":"abc.com",
+//       "password":"abc5678",
+//       "role":"TEACHER"
+//     }
+//   }
+
+// mutation signin($signin:SignInInput){
+//     signIn(signInInput:$signin) {
+//       JWT
+//     }
+//   }
+
+//   {
+//     "signin":{
+//       "username":"emon",
+//       "password":"abc5678"
 //     }
 //   }
